005694c0815cb072b91823cd864351c9bafdb40f
Om Vashishtha <omvashishtha0412@gmail.com>
2026-07-24 16:07:03 +0530

    app.jsx and main.jsx fixes for prod

--- src/app.jsx
+++ src/app.jsx
@@ -1,44 +1,46 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Navigate, Routes, Route } from "react-router-dom";
+
 import LandingPage from "./landing_page/LandingPage";
 import Login from "./login-signup/pages/Login";
 import SignUp from "./login-signup/pages/SignUp";
 import Invite from "./login-signup/pages/Invite";
 
-/* ADMIN MODULE (formerly Member) */
+/* ADMIN MODULE */
 import Dashboard from "./admin/pages/Dashboard";
 import SignYourself from "./admin/pages/SignYourself";
 import Documents from "./admin/pages/Documents";
 import ContactBook from "./admin/pages/ContactBook";
 import Settings from "./admin/pages/Settings";
 import TemplatesPage from "./admin/pages/TemplatesPage";
+
 import useSystemTheme from "./login-signup/hooks/useSystemTheme";
 
 export default function App() {
   useSystemTheme();
+
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* LANDING PAGE */}
-        <Route path="/landing" element={<LandingPage />} />
+    <Routes>
+      {/* LANDING PAGE */}
+      <Route path="/landing" element={<LandingPage />} />
 
-        {/* AUTH ROUTES */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/mail-invite" element={<Invite />} />
+      {/* AUTH ROUTES */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/mail-invite" element={<Invite />} />
 
-        {/* ADMIN ROUTES */}
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/admin" element={<Dashboard />} />
-        <Route path="/admin-dashboard" element={<Dashboard />} />
-        <Route path="/admin-documents" element={<Documents />} />
-        <Route path="/admin-sign-yourself" element={<SignYourself />} />
-        <Route path="/admin-request-signature" element={<SignYourself />} />
-        <Route path="/admin-settings" element={<Settings />} />
-        <Route path="/admin-contact-book" element={<ContactBook />} />
-        <Route path="/admin-templates" element={<TemplatesPage />} />
+      {/* ADMIN ROUTES */}
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/admin" element={<Dashboard />} />
+      <Route path="/admin-dashboard" element={<Dashboard />} />
+      <Route path="/admin-documents" element={<Documents />} />
+      <Route path="/admin-sign-yourself" element={<SignYourself />} />
+      <Route path="/admin-request-signature" element={<SignYourself />} />
+      <Route path="/admin-settings" element={<Settings />} />
+      <Route path="/admin-contact-book" element={<ContactBook />} />
+      <Route path="/admin-templates" element={<TemplatesPage />} />
 
-        <Route path="*" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
+      {/* Redirect unknown routes */}
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   );
 }
